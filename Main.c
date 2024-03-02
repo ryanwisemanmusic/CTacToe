176b979ee6108d8c680309baef777bb7e8b3f70d
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2024-03-01 18:32:08 -0800

    CTacToe V0.7
    Complete build w/ bugs

--- Main.c
+++ Main.c
@@ -18,28 +18,44 @@ void printWinner(char);
 int main()
 {
     char winner = ' ';
+    char response = ' ';
 
-    resetBoard();
+    do
+    {
+        winner = ' ';
+        response = ' ';
+        resetBoard();
 
-    while (winner == '  ' && checkFreeSpaces() != 0)
+    while (winner == ' ' && checkFreeSpaces() != 0)
     {
         printBoard();
 
         playerMove();
         winner = checkWinner();
-        if (winner != '  ' || checkFreeSpaces() == 0)
+        if (winner != ' ' || checkFreeSpaces() == 0)
         {
             break;
         }
 
         computerMove();
         winner = checkWinner();
-        if (winner != '  ' || checkFreeSpaces() == 0)
+        if (winner != ' ' || checkFreeSpaces() == 0)
         {
             break;
-        }
-        
+        }  
     }
+    
+    printBoard();
+      printWinner(winner);
+
+      printf("\nWould you like to play again? (Y/N): ");
+      scanf("%c");
+      scanf("%c", &response);
+      response = toupper(response);
+
+    } while (response == 'Y');
+    
+    printf("Thanks for playing!");
     
     return 0;
 }
@@ -75,7 +91,7 @@ int checkFreeSpaces()
     {
         for (int j = 0; j < 3; j++)
         {
-            if (board[i][j] != '  ')
+            if (board[i][j] != ' ')
             {
                 freeSpaces--;
             }
@@ -91,14 +107,14 @@ void playerMove()
 
     do
     {
-        printf("Enter row #(1-3): ");
+    printf("Enter row #(1-3): ");
     scanf("%d", &x);
     x--;
     printf("Enter column #(1-3): ");
     scanf("%d", &y);
     y--;
 
-    if (board[x][y != '  '])
+    if (board[x][y] != ' ')
     {
         printf("Invalid move!\n");
     }
@@ -107,7 +123,7 @@ void playerMove()
         board[x][y] = PLAYER;
         break;
     }
-    } while (board[x][y] != '  ');
+    } while (board[x][y] != ' ');
 
     
     
@@ -126,7 +142,7 @@ void computerMove()
         {
             x = rand() % 3;
             y = rand() % 3;
-        } while (board[x][y] != '  ');
+        } while (board[x][y] != ' ');
         
         board[x][y] = COMPUTER;
     }
@@ -155,15 +171,15 @@ char checkWinner()
         }
     }
     //check diagonals
-    if (board[0][0] == board[1][1] && board[0][0] == board [2][2])
+    if(board[0][0] == board[1][1] && board[0][0] == board [2][2])
         {
             return board[0][0];
         }
-    if (board[0][2] == board[1][1] && board[0][0] == board [2][0])
+    if(board[0][2] == board[1][1] && board[0][0] == board [2][0])
         {
             return board[0][0];
         }
-    return '  ';
+    return ' ';
 }
 
 void printWinner(char winner)
